8924f44751aadb66612476284437738b160e6c76
Harrison <harrison@theshoebridges.com>
2016-04-20 16:36:18 +1000

    Fix URL

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
   $scope.bots = [];
 
-  $http.get('/api/v1/bots.json')
+  $http.get('api/v1/bots.json')
     .then(function(res) {
       $scope.bots = res.data.bots;
     });
